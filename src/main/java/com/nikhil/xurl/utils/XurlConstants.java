49b7457be8274da90c5daf8d5755536bd067b66c
Nikhil Dabhade <nikhilkd1999@gmail.com>
2023-04-01 00:45:28 +0530

    Chenged URL Prefix to "x-url.onrender.com/"

--- src/main/java/com/nikhil/xurl/utils/XurlConstants.java
+++ src/main/java/com/nikhil/xurl/utils/XurlConstants.java
@@ -2,7 +2,7 @@ package com.nikhil.xurl.utils;
 
 public class XurlConstants {
 
-	public final static String URL_PREFIX = "https://x-url.onrender.com/";
+	public final static String URL_PREFIX = "x-url.onrender.com/";
 	public final static int SHORT_URL_HASH_LENGTH = 6;
 
 }
