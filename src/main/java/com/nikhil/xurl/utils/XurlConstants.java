80d5b9e5dd2a53862cfc00e106386f883da3b46d
Nikhil Dabhade <nikhilkd1999@gmail.com>
2023-04-01 18:56:44 +0530

    Implemented sonarlint suggestions.

--- src/main/java/com/nikhil/xurl/utils/XurlConstants.java
+++ src/main/java/com/nikhil/xurl/utils/XurlConstants.java
@@ -2,7 +2,10 @@ package com.nikhil.xurl.utils;
 
 public class XurlConstants {
 
-	public final static String URL_PREFIX = "x-url.onrender.com/";
-	public final static int SHORT_URL_HASH_LENGTH = 6;
+	private XurlConstants() {
+	}
+
+	public static final String URL_PREFIX = "x-url.onrender.com/";
+	public static final int SHORT_URL_HASH_LENGTH = 6;
 
 }
